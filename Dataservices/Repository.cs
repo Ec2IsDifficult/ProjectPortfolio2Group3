e9916d6b70d2ae4564851d0ab939975bf129ab2a
Nils Rasmus Armstrong <niraar@ruc.dk>
2021-11-07 11:09:32 +0100

    Created the implementaion of the IRepository. This repository is completely gerenic because it emulates the CRUD functionality

---
+++ Dataservices/Repository.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dataservices
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        //Since the specific repositories need to inherit the functionality we need protected not private
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+        
+        public TEntity Get(int id)
+        {
+            //The .Set function return a DbSet<TEntity> for access by subclasses (specific repositories)
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddAll(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Set<TEntity>().Update(entity);
+        }
+
+        public void UpdateAll(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().UpdateRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void DeleteAll(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
